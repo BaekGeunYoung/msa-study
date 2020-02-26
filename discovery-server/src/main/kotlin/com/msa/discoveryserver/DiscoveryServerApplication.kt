57dc92bf42febebd1227055be561677e4358d541
BaekGeunYoung <dvmflstm@naver.com>
2020-02-26 23:10:59 +0900

    add eureka server & add simple controller in microservice

---
+++ discovery-server/src/main/kotlin/com/msa/discoveryserver/DiscoveryServerApplication.kt
@@ -0,0 +1,13 @@
+package com.msa.discoveryserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class DiscoveryServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DiscoveryServerApplication>(*args)
+}
--- microservice-a/src/main/kotlin/com/msa/microservicea/controller/GreetingsController.kt
+++ microservice-a/src/main/kotlin/com/msa/microservicea/controller/GreetingsController.kt
@@ -1,2 +1,14 @@
 package com.msa.microservicea.controller
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingsController {
+    @Value("\${microservice.example.greetings}")
+    private lateinit var greetings: String
+
+    @GetMapping("/greeting")
+    fun greetings() = greetings
+}
