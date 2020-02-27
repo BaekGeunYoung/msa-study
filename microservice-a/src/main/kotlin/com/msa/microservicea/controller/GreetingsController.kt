fb38ec27590d1bc9229196ffe3823f762a18d7cd
BaekGeunYoung <dvmflstm@naver.com>
2020-02-27 15:55:39 +0900

    fix sample controller of microservice other1

--- microservice-a/src/main/kotlin/com/msa/microservicea/controller/GreetingsController.kt
+++ microservice-a/src/main/kotlin/com/msa/microservicea/controller/GreetingsController.kt
@@ -1,15 +1,11 @@
 package com.msa.microservicea.controller
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GreetingsController {
-    @Value("\${microservice.example.greetings}")
-    private lateinit var greetings: String
-
     @GetMapping("/greeting")
-    fun greetings(@RequestHeader("Custom") custom: String) = custom
+    fun greetings(@RequestHeader("username") username: String) = username
 }
