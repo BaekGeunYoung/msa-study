cacc40a5012abe90c31fc8fa4b47f8779cabe12d
BaekGeunYoung <dvmflstm@naver.com>
2020-03-06 16:58:32 +0900

    exercise microservice 생성

---
+++ microservice-excercise/src/main/kotlin/com/msa/excercise/ExcerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.msa.excercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcerciseApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExcerciseApplication>(*args)
+}
---
+++ microservice-excercise/src/test/kotlin/com/msa/excercise/ExcerciseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.msa.excercise
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcerciseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
