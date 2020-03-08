acf1a57794597f7b1ed263ec9cd2709efc61a42a
BaekGeunYoung <dvmflstm@naver.com>
2020-03-08 21:32:03 +0900

    API gateway에서 user db에 직접 접근할 수 있도록 하기 위해 jpa 의존성 모듈 추가

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -20,6 +20,7 @@ extra["springCloudVersion"] = "Hoxton.SR1"
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
