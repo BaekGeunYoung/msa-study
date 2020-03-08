efa06894fae7524f8bb83d789f938ae4c626a08e
BaekGeunYoung <dvmflstm@naver.com>
2020-03-08 20:44:24 +0900

    feign client 사용을 위한 의존성 모듈 추가

--- microservice-excercise/build.gradle.kts
+++ microservice-excercise/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
