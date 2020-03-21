8bcd19aa0bd159ef39a99a88bb602720a0999d4f
BaekGeunYoung <dvmflstm@naver.com>
2020-03-22 01:29:26 +0900

    여러 컴포넌트에서 공유하는 type들을 모아놓기 위한 파일 생성

---
+++ front/src/type/index.tsx
@@ -0,0 +1,15 @@
+export type ExerciseDetail = {
+    name: string
+    weight: number
+    rep: number
+    set: number
+    volume: number
+}
+
+export type DietDetail = {
+    name: string
+    calorie: number
+    C: number
+    P: number
+    F: number
+}
