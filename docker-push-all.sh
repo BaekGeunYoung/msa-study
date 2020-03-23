fcc8a0c645cc891c82e149b5e18c50008faa9595
Your Name <you@example.com>
2020-03-23 14:24:20 +0900

    make all-in-one docker image push script

---
+++ docker-push-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker push dvmflstm/config:latest
+docker push dvmflstm/gateway:latest
+docker push dvmflstm/auth:latest
+docker push dvmflstm/exercise:latest
+docker push dvmflstm/diet:latest
+docker push dvmflstm/statistics:latest
+
