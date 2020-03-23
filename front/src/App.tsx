b0166d721cc77c5a9b4aea6c9a9f929ae3d619e6
BaekGeunYoung <dvmflstm@naver.com>
2020-03-23 10:26:20 +0900

    resolve conflicts

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import StartPage from "./page/StartPage";
+import MainPage from "./page/MainPage";
 
 function App() {
   return (
       <BrowserRouter>
           <Switch>
               <Route exact path={"/"} component={StartPage} />
+              <Route exact path={"/main"} component={MainPage} />
           </Switch>
       </BrowserRouter>
   );
