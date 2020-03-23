029bb6cee31e462e3d14f8e7c306cdb6dd69a00a
Your Name <you@example.com>
2020-03-23 14:23:55 +0900

    change content of docker image build script

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker image build -t msa-practice/config ./config-server
-docker image build -t msa-practice/registry ./discovery-server
-docker image build -t msa-practice/auth ./microservice-auth
-docker image build -t msa-practice/diet ./microservice-diet
-docker image build -t msa-practice/exercise ./microservice-excercise
-docker image build -t msa-practice/statistics ./microservice-statistics
+docker image build -t dvmflstm/config ./config-server
+docker image build -t dvmflstm/registry ./discovery-server
+docker image build -t dvmflstm/auth ./microservice-auth
+docker image build -t dvmflstm/diet ./microservice-diet
+docker image build -t dvmflstm/exercise ./microservice-excercise
+docker image build -t dvmflstm/statistics ./microservice-statistics
+docker image build -t dvmflstm/gateway ./gateway
 
